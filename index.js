c0e3e8fd5880cb4f80c3eeb163e8a4117d7942ce
Allan.W.C <allan_195@hotmail.com>
2020-04-20 17:53:39 +1000

    missing greater than sign

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.set("trust proxy", 1);
 
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000,
-  max: 1000,
+  max: 100,
 });
 
 app.use(limiter);
--- src/routes/recipeRoutes.js
+++ src/routes/recipeRoutes.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Recipe = require("../models/Recipe");
+const requireAuth = require("../middlewares/requireAuth");
 
 const router = express.Router();
 
-router.get("/recipes", async(req, res) = {
+// router.use(requireAuth);
+
+router.get("/recipes", async (req, res) => {
   const recipes = await Recipe.find({ userId: req.user._id });
   res.send(recipes);
 });
 
-router.post('/recipes', async (req, res) => {
+router.post("/recipes", async (req, res) => {
   const { userId, recipe } = req.body;
 
-  if(!recipe) {
-    return res.status(422).send({ error: 'You must provide a recipe' });
+  if (!recipe) {
+    return res.status(422).send({ error: "You must provide a recipe" });
   }
   try {
     const recipe = new Recipe({ recipe, userId: req.user._id });
